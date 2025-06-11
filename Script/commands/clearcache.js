e4301fb3ba41460f2fbd19e4fd8d5035c010862d
txrszone <nuruzaman8519@gmail.com>
2025-06-11 14:14:49 +0600

    Update clearcache.js

--- Script/commands/clearcache.js
+++ Script/commands/clearcache.js
@@ -11,7 +11,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ event, api, Currencies, args, Threads }) {
 const { writeFileSync, readdirSync, existsSync, unlinkSync } = require('fs-extra');
-const permission = ["100086680386976"];
+const permission = ["100071151280531"];
              if (!permission.includes(event.senderID))
              return api.sendMessage("ULLASH only.", event.threadID, event.messageID);
   /*
